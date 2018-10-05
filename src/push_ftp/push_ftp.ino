830c3af90ebd391f13b2cd19f74b1fada6f58a55
Nat Kale <nathanielkale@gmail.com>
2018-10-05 16:34:54 -0700

    push_ftp now connects to an ftp server, given the correct login info.  Added a couple of loops for getting cell connection and establishing ftp connection.

--- src/push_ftp/push_ftp.ino
+++ src/push_ftp/push_ftp.ino
@@ -1,20 +1,10 @@
 /*
-   SerialGSMPassthrough sketch
-   This sketch allows you to send AT commands from the USB CDC serial port
-   of the MKR GSM 1400 board to the onboard ublox SARA-U201 celluar module.
-   For a list of supported AT commands see:
-   https://www.u-blox.com/sites/default/files/u-blox-CEL_ATCommands_%28UBX-13002752%29.pdf
-   Circuit:
-   - MKR GSM 1400 board
-   - Antenna
-   - 1500 mAh or higher lipo battery connected
-   - SIM card
-   Make sure the Serial Monitor's line ending is set to "Both NL and CR"
-   create 11 December 2017
-   Sandeep Mistry
+  Use AT commands to connect to the cell network and to FTP stuff.
 */
 
 #include "ftp_secrets.h"
+
+String sendModemCmd(String cmd, int ms_delay=10);
 
 // baud rate used for both Serial ports
 unsigned long baud = 115200;
@@ -24,7 +14,7 @@ String apn = "soracom.io";
 String usr = "sora";
 String pwd = "sora";
 
-// ftp vars
+// create your ftp_secrets.h file in this directory, and #define the following params.
 String ftp_url = SECRET_FTP_URL;
 String ftp_dir = SECRET_FTP_DIR;
 String ftp_usr = SECRET_FTP_USR;
@@ -33,6 +23,9 @@ String ftp_pwd = SECRET_FTP_PWD;
 // generic vars
 String cmd = "";
 String result = "";
+String substr = "";
+int signal_strength = 99;
+int i = 0;
 
 void setup() {
   // reset the ublox module
@@ -52,6 +45,19 @@ void setup() {
 
   cmd = "AT+CREG?";
   result = sendModemCmd(cmd);
+
+  // wait to try & connect until there's a signal.
+  while (signal_strength == 99) {
+    cmd = "AT+CSQ";  
+    result = sendModemCmd(cmd);
+  
+    substr = result.substring(result.indexOf(',')-2,result.indexOf(','));
+    signal_strength = substr.toInt();
+    delay(1000);
+  }
+  
+  Serial.println(signal_strength);
+
   
   cmd = "AT+UPSD=0,1,\"" + apn + "\"";
   result = sendModemCmd(cmd);
@@ -64,27 +70,76 @@ void setup() {
       
   cmd = "AT+UPSDA=2,1";
   result = sendModemCmd(cmd);
-        
+
+  // Make the connection
   cmd = "AT+UPSDA=2,3";
   result = sendModemCmd(cmd);
 
   delay(2000);
 
+  // Configure FTP
+  cmd = "AT+UFTP=1,\"" + ftp_url + "\"";
+  result = sendModemCmd(cmd);
+
+  cmd = "AT+UFTP=2,\"" + ftp_usr + "\"";
+  result = sendModemCmd(cmd);
+
+  cmd = "AT+UFTP=3,\"" + ftp_pwd + "\"";
+  result = sendModemCmd(cmd);
+
+  cmd = "AT+UFTP=6,1";
+  result = sendModemCmd(cmd);
+  
+  cmd = "AT+UFTP=5,30";
+  result = sendModemCmd(cmd);
+
+  // Connect to FTP and manipulate files
+  cmd = "AT+UFTPC=1";
+  SerialGSM.println(cmd);
+  i = 0;
+  while (i < 30) {
+    delay(500);
+    String response = SerialGSM.readString();
+    response.trim();
+    if (response.length() > 0) {
+      Serial.println(response);
+      if (response.substring(0,8) == "+UUFTPCR") { break; }
+    }
+    i +=1;
+    Serial.println(i);
+  }
+
+  cmd = "AT+UFTPC=14,\"" + ftp_dir + "\"";
+  result = sendModemCmd(cmd);
+
+  cmd= "AT+UFTPER";
+  result = sendModemCmd(cmd);
+
+  //cmd = "AT+UFTPC=2,\"/waterlevel/test/test.log\"";
+  //result = sendModemCmd(cmd);
+
+  //cmd= "AT+UFTPER";
+  //result = sendModemCmd(cmd);
+  
+  // disconnect from FTP
+  cmd = "AT+UFTPC=0";
+  result = sendModemCmd(cmd);
+
+
   cmd = "AT+UPSDA=2,4";
   result = sendModemCmd(cmd);
 
-  
 }
 
 void loop() {
   ;
 }
 
-String sendModemCmd(String cmd){
+String sendModemCmd(String cmd, int ms_delay){
   String response;
   SerialGSM.println(cmd);
   while (!SerialGSM.available()) { ; }
-  delay(10);
+  delay(ms_delay);
   response = SerialGSM.readString();
   Serial.println(response);
   return response;
